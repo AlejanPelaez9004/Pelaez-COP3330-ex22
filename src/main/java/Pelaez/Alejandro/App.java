92cd6b853d68ed00c1f013a17fc0497aabf726f2
AlejanPelaez9004 <alejanpelaez@knights.ucf.edu>
2021-09-12 16:05:27 -0700

    Complete

--- src/main/java/Pelaez/Alejandro/App.java
+++ src/main/java/Pelaez/Alejandro/App.java
@@ -8,10 +8,31 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args)
     {
-        System.out.print("What is your name?");
         Scanner input = new Scanner(System.in);
-        String name = input.next();
-        System.out.print("Hello " + name + ", nice to meet you!");
-        System.out.print("\n");
+        System.out.print("Enter the first number: ");
+        int one = input.nextInt();
+        System.out.print("Enter the second number: ");
+        int two = input.nextInt();
+        System.out.print("Enter the third number: ");
+        int three = input.nextInt();
+        int largestNum = 0;
+
+        if (one != two && two != three && three != one)
+        {
+            if (one > two && one > three)
+                largestNum = one;
+            if (two > one && two > three)
+                largestNum = two;
+            if (three > one && three > two)
+                largestNum = three;
+        }
+        else
+        {
+            System.out.println("Numbers are the same");
+            return;
+        }
+
+
+        System.out.println("The largest number is " + largestNum);
     }
 }
